438295c0718e1f03a52d994fbdb8a612575d6912
Yash Kumar Kalyan <82755153+yashkumarkalyan@users.noreply.github.com>
2021-10-13 19:25:35 +0530

    get error fix

--- index.js
+++ index.js
@@ -17,9 +17,15 @@ const labels = [
   "Rohit",
 ];
 Promise.all([
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri(
+    "/https://github.com/yashkumarkalyan/attendance-face-recognisation/tree/main/models"
+  ),
+  faceapi.nets.faceLandmark68Net.loadFromUri(
+    "/https://github.com/yashkumarkalyan/attendance-face-recognisation/tree/main/models"
+  ),
+  faceapi.nets.ssdMobilenetv1.loadFromUri(
+    "/https://github.com/yashkumarkalyan/attendance-face-recognisation/tree/main/models"
+  ),
 ]).then(start);
 
 async function start() {
